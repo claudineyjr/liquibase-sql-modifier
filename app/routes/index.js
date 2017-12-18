0233e07a06af7c9a9f6b8af8fee8a62995b9506b
Claudiney Junior <claudineyjunior@claudineys-air.bs.br.bsch>
2017-12-18 18:20:37 -0200

    Mensagem de sucesso adicionada

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,10 @@
 module.exports = (application, req, res) => {
   application.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { message: {} });
   });
 
   application.post('/file-modifier', (req, res) => {
     application.app.controllers.fileModifier.modify(application, req, res);
+    res.render('index', { message: [{ msg: 'Arquivos gerados com sucesso!' }] })
   });
 }
