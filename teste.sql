aea7a1b91d9f08f2e459ecd2b64eec70342cacbf
Claudiney Junior <claudineyjunior@claudineys-air.bs.br.bsch>
2017-12-18 17:59:33 -0200

    Delete unnused files

--- teste.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `123` SET `gato` = 'abeia' where id = 1;
-UPDATE `123` SET `gato` = 'vaca' where id = 2;
-UPDATE `123` SET `gato` = 'pudim' where id = 3;
--- variaveis.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  matricula: 'X199114', // seu id no sistema
-  numeroInicial: 0, // numero inicial desejado
-  nomeDoArquivo: './scripts/17.MOR-home-and-menu-fix.sql', // nome do arquivo origem
-  outputFile: './out/17.MOR-home-and-menu-fix.sql'
-}
